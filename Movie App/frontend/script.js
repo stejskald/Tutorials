f7be26d1b5e037fd58cecca07b03f67649c75e16
David Stejskal <david.stej@seznam.cz>
2024-09-22 16:37:35 +0200

    Reindented frontend to 2 spaces and deleted an image

--- Movie App/frontend/script.js
+++ Movie App/frontend/script.js
@@ -6,51 +6,51 @@ const main = document.getElementById("section")
 const form = document.getElementById("form")
 const search = document.getElementById("query")
 
-returnMovies(APILINK)
+returnReviews(APILINK)
 
-function returnMovies(url) {
-    fetch(url).then(res => res.json())
-    .then(function(data) {
-        console.log(data.results)
-        data.results.forEach(element => {
-            const divCard = document.createElement("div")
-            divCard.setAttribute("class", "card")
-
-            const divRow = document.createElement("div")
-            divRow.setAttribute("class", "row")
-
-            const divCol = document.createElement("div")
-            divCol.setAttribute("class", "column")
-
-            const image = document.createElement("img")
-            image.setAttribute("class", "thumbnail")
-            image.setAttribute("id", "image")
-
-            const titleH3 = document.createElement("h3")
-            titleH3.setAttribute("id", "title")
-            // const center = document.createElement("center") // <center> is deprecated
-
-            titleH3.innerHTML = `${element.title}`
-            image.src = IMG_PATH + element.poster_path
-
-            divCard.appendChild(image)
-            divCard.appendChild(titleH3)
-            divCol.appendChild(divCard)
-            divRow.appendChild(divCol)
-
-            main.appendChild(divRow)
-        })
+function returnReviews(url) {
+  fetch(url).then(res => res.json())
+  .then(function(data) {
+    console.log(data.results)
+    data.results.forEach(element => {
+      const divCard = document.createElement("div")
+      divCard.setAttribute("class", "card")
+      
+      const divRow = document.createElement("div")
+      divRow.setAttribute("class", "row")
+      
+      const divCol = document.createElement("div")
+      divCol.setAttribute("class", "column")
+      
+      const image = document.createElement("img")
+      image.setAttribute("class", "thumbnail")
+      image.setAttribute("id", "image")
+      
+      const titleH3 = document.createElement("h3")
+      titleH3.setAttribute("id", "title")
+      // const center = document.createElement("center") // <center> is deprecated
+      
+      titleH3.innerHTML = `${element.title}<br><a href="movie.html?id=${element.id}&title=${element.title}">Movie reviews</a>`
+      image.src = IMG_PATH + element.poster_path
+      
+      divCard.appendChild(image)
+      divCard.appendChild(titleH3)
+      divCol.appendChild(divCard)
+      divRow.appendChild(divCol)
+      
+      main.appendChild(divRow)
     })
+  })
 }
 
 form.addEventListener("submit", (ev) => {
-    ev.preventDefault()
-    main.innerHTML = ""
-
-    const searchItem = search.value
-
-    if (searchItem) {
-        returnMovies(SEARCHAPI + searchItem)
-        search.value = ""
-    }
+  ev.preventDefault()
+  main.innerHTML = ""
+  
+  const searchItem = search.value
+  
+  if (searchItem) {
+    returnReviews(SEARCHAPI + searchItem)
+    search.value = ""
+  }
 })
