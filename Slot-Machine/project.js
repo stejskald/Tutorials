c149f93dead088656dcb284d0a955e743349c3be
David Stejskal <david.stej@seznam.cz>
2024-09-25 08:55:59 +0200

    Corrected the playAgain prompt ending

--- Slot-Machine/project.js
+++ Slot-Machine/project.js
@@ -166,7 +166,7 @@ const game = () => {
       break;
     }
 
-    const playAgain = prompt("Do you want to play again (y/n)?\n");
+    const playAgain = prompt("Do you want to play again (y/n)? ");
     if (playAgain != "y") break;
   }
 }
