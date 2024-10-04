6645ffc2746c88adbdd410e739fe047a30ff2037
David Stejskal <david.stej@seznam.cz>
2024-10-04 15:22:23 +0200

    Vertical and horizontal FlatList mechanism work

--- React-Native-Mobile-App/components/Trending.jsx
+++ React-Native-Mobile-App/components/Trending.jsx
@@ -1,15 +1,88 @@
-import { View, Text, FlatList } from 'react-native';
-import React from 'react';
+import { Text, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native';
+import React, { useState } from 'react';
+import * as Animatable from "react-native-animatable";
+import { icons } from '../constants';
 
-const Trending = ({posts}) => {
+const zoomIn = {
+  0: {
+    scale: 0.8
+  },
+  1: {
+    scale: 1
+  }
+}
+
+const zoomOut = {
+  0: {
+    scale: 1
+  },
+  1: {
+    scale: 0.8
+  }
+}
+
+const TrentindItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+
+  // console.log(activeItem.$id, item.$id);
+
   return (
-    <FlatList 
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        // TODO continue here
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <TouchableOpacity
+          activeOpacity={0.6}
+          className="relative justify-center items-center"
+          onPress={() => setPlay(true)}
+        >
+          <ImageBackground
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black-100"
+            resizeMode='cover'
+            source={{ uri: item.thumbnail }}
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+
+      )}
+    </Animatable.View>
+  )
+}
+
+const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[1]);
+
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key)
+    }
+  }
+
+  return (
+    <FlatList
       data={posts}
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <Text className="text-3xl text-white">{item.id}</Text>
+        <TrentindItem
+          activeItem={activeItem}
+          item={item}
+        />
       )}
       horizontal
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70
+      }}
+      contentOffset={{ x: 150 }}
     />
   );
 }
