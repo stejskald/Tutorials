6f49bdb5809f1c5389ff8162f1ac4ee6d3af6266
David Stejskal <david.stej@seznam.cz>
2024-10-06 14:39:41 +0200

    Updated hardcoded sections with gotten data from database

--- React-Native-Mobile-App/app/(tabs)/home.jsx
+++ React-Native-Mobile-App/app/(tabs)/home.jsx
@@ -8,8 +8,10 @@ import EmptyState from '../../components/EmptyState'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const Home = () => {
+  const { user, setUser, setIsLogged } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
 
@@ -39,11 +41,11 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome back
+                  Welcome back,
                 </Text>
-                
+
                 <Text className="text-2xl font-psemibold text-white">
-                  JS Mastery
+                  {user?.username}
                 </Text>
               </View>
 
