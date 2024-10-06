f5237f62736684c2f5ba8d5b96942e3ebaa063d9
David Stejskal <david.stej@seznam.cz>
2024-10-06 14:42:07 +0200

    Implemented Search page by reusing the Home page and updated the components/SearchInput to be able search with query

--- React-Native-Mobile-App/app/search/[query].jsx
+++ React-Native-Mobile-App/app/search/[query].jsx
@@ -1,14 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SearchInput from '../../components/SearchInput'
+import EmptyState from '../../components/EmptyState'
+import { searchPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
+import { useLocalSearchParams } from 'expo-router'
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+
+  // console.log(query, posts);
+
+  useEffect(() => {
+    refetch();
+    // will be called every time that the query changes
+  }, [query])
+
+  // console.log(posts);
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        // data={[]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          // <Text className="text-3xl text-white">{item.title}</Text>
+          <VideoCard video={item} />
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Search results
+            </Text>
+
+            <Text className="text-2xl font-psemibold text-white">
+              {query}
+            </Text>
+
+            <View className="mt-6 mb-8" >
+              {/* initialQuery - will be auto populated with query we filled in home page */}
+              <SearchInput initialQuery={query} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found..."
+            subtitle="No videos found for this search query!"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default Search
-
-const styles = StyleSheet.create({})
